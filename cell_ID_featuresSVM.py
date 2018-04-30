b9c438a3e514facb9891d8657d1f67be83e014d2
devins <devsullivan@gmail.com>
2018-04-30 14:32:31 +0200

    added pca for speed and fixing predictor.

--- cell_ID_featuresSVM.py
+++ cell_ID_featuresSVM.py
@@ -13,16 +13,24 @@ import os
 import glob 
 from sklearn import svm 
 from sklearn.ensemble import RandomForestClassifier
+from scipy import stats
+from sklearn.decomposition import PCA
 
 
-def write_output(img_list,label_list):
+def write_output(img_list,label_list,outpath):
+    with open(outpath,'wb') as outfile:
+        resultwriter = csv.writer(outfile,delimiter=',')
+        for img,label in zip(img_list,label_list):
+            resultwriter.writerow([img,label])
+    
 
 def main():
     
     #Parameters 
-    featpath = '/Users/devinsullivanMBP/cell_ID_hackathon/features/'
+    featpath = '/Users/devinsullivanMBP/cell_ID_hackathon/training_small/'
     labelpath = '/Users/devinsullivanMBP/cell_ID_hackathon/training_upload.csv'
-    validation_featpath = '/Users/devinsullivanMBP/cell_ID_hackathon/validation_features2/' 
+    validation_featpath = '/Users/devinsullivanMBP/cell_ID_hackathon/validation_features/' 
+    outpath = '/Users/devinsullivanMBP/cell_ID_hackathon/validation_predictions.csv'
     
     cell_lines = {}
     with open(labelpath,'r') as labelfile:
@@ -38,7 +46,7 @@ def main():
     num_lines = []
     name_list = []
     label_list = []
-    feats = np.empty((1,2233))
+    #feats = np.empty((1,2233))
 
     
     #go through files 
@@ -47,48 +55,82 @@ def main():
        #get features and number of lines in the file
        currfeats = np.loadtxt(open(filename, "rb"), delimiter=",", skiprows=0)
        print(np.shape(currfeats))
-       feats = np.vstack((feats,currfeats))
+       if 'feats' in locals():
+           feats = np.vstack((feats,currfeats))
+       else:
+           feats = currfeats
+               
        currlines = np.shape(currfeats)
        num_lines.append(currlines[0])
        name_list.append([imgname]*num_lines[-1])
-       label_list.append([cell_lines[imgname]]*num_lines[-1])
-
-    print(np.shape(feats))
+       currnames = [cell_lines[imgname]]*num_lines[-1]
+       label_list.extend(currnames)
+       
+    
+    #normalize features with zscore 
+    mufeats = np.mean(feats,axis=0)
+    stdfeats = np.std(feats,axis=0)
+    zfeats = stats.zscore(feats)
+    #remove any columns that are always nan
+    nan_cols = ~np.any(np.isnan(zfeats), axis=0)
+    zfeats = zfeats[:,nan_cols]
+    inf_cols = ~np.any(np.isinf(zfeats), axis=0)
+    zfeats = zfeats[:,inf_cols]
+    
+    #perform Principal Components Analysis to reduce dimension
+    #randomly choosing 5 components for now. 
+    pca = PCA(n_components=5)
+    pca.fit(zfeats)
+    zf_pca5 = pca.transform(zfeats)
+    
+    print(np.shape(zf_pca5))
+    
     #Try a classifier for literally only the number of lines (cells)
-
     num_lines = np.asarray(num_lines)
     num_lines = num_lines.reshape(-1,1)
     num_imgs = len(num_lines) 
-    #init
-    numline_classifier = svm.SVC()
+
+    #init svm
+    features_classifier = svm.SVC()
+    #numline_classifier = svm.SVC()
     #train
-    numline_classifier.fit(feats,label_list)
+    print(len(label_list))
+    features_classifier.fit(zf_pca5,label_list)
+    #numline_classifier.fit(num_lines,label_list)
     #predict
     train_accuracy = np.zeros(num_imgs)
-    for i,(cellcount,label) in enumerate(zip(feats,label_list)):
-        print(label)
-        print(cellcount)
-        curr_pred = numline_classifier.predict(cellcount[0])
-        train_accuracy[i] = curr_pred==label
-    print('training accuracy: ')
-    print(np.sum(train_accuracy)/num_imgs)
+    pred = features_classifier.predict(zf_pca5)
+    acc = pred==label_list
+    print('training accuracy per-cell: ')
+    print(np.sum(acc)/np.sum(num_lines))
+
     
     #read the validation set
     num_lines_validation = []
     testfeats = np.empty((1,2233))
     pred_list = []
     name_list_validation = []
-    for filename in glob.iglob(validation_featpath+'**/*features.csv', recursive=True):
+    for filename in glob.iglob(validation_featpath+'*features.csv', recursive=True):
        imgname = os.path.basename(os.path.dirname(filename))
        #get features and number of lines in the file
        curr_testfeats = np.loadtxt(open(filename, "rb"), delimiter=",", skiprows=0)
-       print(np.shape(curr_testfeats))
-       testfeats = np.vstack((testfeats,curr_testfeats))
+       #normalize with training z-score
+       ztestfeats = (curr_testfeats-mufeats)/stdfeats
+       #remove nan *then* inf (order is important)
+       ztestfeats = ztestfeats[:,nan_cols]
+       ztestfeats = ztestfeats[:,inf_cols]
+       ztest_pca5 = pca.transform(ztestfeats)
+       #predict validation labels 
+       cell_pred = features_classifier.predict(ztest_pca5)
+       print(cell_pred)
+       curr_pred = stats.mode(cell_pred)
+       print(np.shape(ztestfeats))
+       print(curr_pred[0])
+       #testfeats = np.vstack((testfeats,curr_testfeats))
        currlines = np.shape(curr_testfeats)
        num_lines_validation.append(currlines[0])
        name_list_validation.append([imgname]*num_lines[-1])
-       #predict validation labels 
-       curr_pred = numline_classifier.predict(testfeats)
+
        pred_list.append(curr_pred)
        name_list.append(imgname)
        #label_list.append(cell_lines[imgname])
@@ -97,14 +139,8 @@ def main():
     
     
     #write output 
+    #write_output(name_list_validation,pred_list,outpath)
     
-    cell_lines = {}
-    with open(labelpath,'r') as validationfile:
-        labelreader = csv.reader(labelfile,delimiter=',')
-        labelreader.__next__()
-        for row in labelreader:
-            cell_lines[row[0]]= row[1]
-
     
     
 
